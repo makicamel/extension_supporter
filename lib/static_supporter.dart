9d33b24548f7a61f670eee0110d8cac03738f9c6
makicamel <unright@gmail.com>
2019-11-04 18:43:11 +0900

    Add example about DateTime#change

--- lib/static_supporter.dart
+++ lib/static_supporter.dart
@@ -3,6 +3,13 @@ library static_supporter;
 extension StaticSupporterDateTime on DateTime {
   /// Recieve argument(s) and return new instance changed only recieved parameters' value.
   /// If no arguments passed, return same value instance.
+  /// DateTime(2020, 12, 31).change(year: 2001) => 2001-12-31 00:00:00.000
+  /// ** notice **
+  ///   If there's no corresponding date after change,
+  ///   Dart convert the date-time to correct.
+  /// DateTime(2020, 2, 29).change(year: 2021) => 2021-03-01 00:00:00.000
+  /// DateTime(2020, 11, 12).change(month: 13) => 2021-01-12 00:00:00.000
+  /// DateTime(2020, 5, 4).change(month: -1)   => 2019-11-04 00:00:00.000
   DateTime change({
     int year,
     int month,
