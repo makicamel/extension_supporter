f7c7090c721d40574a7a91c4f3833625e5c1a807
makicamel <unright@gmail.com>
2019-11-04 18:30:41 +0900

    Implement DateTime#change method

--- lib/static_supporter.dart
+++ lib/static_supporter.dart
@@ -1,7 +1,27 @@
 library static_supporter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+extension StaticSupporterDateTime on DateTime {
+  /// Recieve argument(s) and return new instance changed only recieved parameters' value.
+  /// If no arguments passed, return same value instance.
+  DateTime change({
+    int year,
+    int month,
+    int day,
+    int hour,
+    int minute,
+    int second,
+    int millisecond,
+    int microsecond,
+  }) {
+    return DateTime(
+      year ?? this.year,
+      month ?? this.month,
+      day ?? this.day,
+      hour ?? this.hour,
+      minute ?? this.minute,
+      second ?? this.second,
+      millisecond ?? this.millisecond,
+      microsecond ?? this.microsecond,
+    );
+  }
 }
--- test/static_supporter_test.dart
+++ test/static_supporter_test.dart
@@ -2,11 +2,47 @@ import 'package:test/test.dart';
 import 'package:static_supporter/static_supporter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('DateTime#change', () {
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(year: 2001),
+        DateTime(2001, 1, 2, 10, 20, 30, 4, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(month: 2),
+        DateTime(2000, 2, 2, 10, 20, 30, 4, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(day: 3),
+        DateTime(2000, 1, 3, 10, 20, 30, 4, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(hour: 11),
+        DateTime(2000, 1, 2, 11, 20, 30, 4, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(minute: 21),
+        DateTime(2000, 1, 2, 10, 21, 30, 4, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(second: 31),
+        DateTime(2000, 1, 2, 10, 20, 31, 4, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(millisecond: 5),
+        DateTime(2000, 1, 2, 10, 20, 30, 5, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(microsecond: 6),
+        DateTime(2000, 1, 2, 10, 20, 30, 4, 6));
+  });
+
+  test('DateTime#change accepts multiple arguments', () {
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(year: 2001, month: 2),
+        DateTime(2001, 2, 2, 10, 20, 30, 4, 5));
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(hour: 11, minute: 21),
+        DateTime(2000, 1, 2, 11, 21, 30, 4, 5));
+    expect(
+        DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(
+          year: 1999,
+          month: 12,
+          day: 13,
+          hour: 14,
+          minute: 15,
+          second: 16,
+          millisecond: 17,
+          microsecond: 18,
+        ),
+        DateTime(1999, 12, 13, 14, 15, 16, 17, 18));
+  });
+
+  test('DateTime#change returns same value instance with passing no arguments',
+      () {
+    expect(DateTime(2000, 1, 2, 10, 20, 30, 4, 5).change(),
+        DateTime(2000, 1, 2, 10, 20, 30, 4, 5));
   });
 }
