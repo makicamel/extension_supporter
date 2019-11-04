30fb5fd5eecdd641c8d503c6315cab6403ad5b3d
makicamel <unright@gmail.com>
2019-11-04 18:59:39 +0900

    Move DateTime extensions to date_time.dart

--- test/date_time_test.dart
+++ test/date_time_test.dart
@@ -1,5 +1,5 @@
 import 'package:test/test.dart';
-import 'package:static_supporter/static_supporter.dart';
+import 'package:static_supporter/date_time.dart';
 
 void main() {
   test('DateTime#change', () {
